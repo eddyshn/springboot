374cccbda6bf95d3ded6ac3917c69b7024dd0afe
eddyshn <eddy.shan@outlook.com>
2019-08-19 22:45:50 +0800

    javassm

--- javassm/src/main/java/org/javaboy/config/SpringMVCConfig.java
+++ javassm/src/main/java/org/javaboy/config/SpringMVCConfig.java
@@ -1,13 +1,42 @@
 package org.javaboy.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.javaboy.interceptor.MyInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Configuration
 @ComponentScan(basePackages = "org.javaboy",useDefaultFilters = false,
         includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Controller.class),
                 @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)})
-public class SpringMVCConfig {
+public class SpringMVCConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor()).addPathPatterns("/**");
+    }
+
+    @Bean
+    MyInterceptor myInterceptor() {
+        return new MyInterceptor();
+    }
+
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        converters.add(converter);
+    }
 }
--- javassm/src/main/java/org/javaboy/controller/HelloController.java
+++ javassm/src/main/java/org/javaboy/controller/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
 
@@ -14,5 +17,14 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return hellloService.sayHello();
+    }
+
+    @GetMapping("/data")
+    public List<String> getData() {
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            list.add("www.justdojava.com>>>" + i);
+        }
+        return list;
     }
 }
